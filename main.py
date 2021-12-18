7443a20442b27bae8d2ec1de28a1ad1a5378b561
uliana <val.uliana@yandex.ru>
2021-12-18 16:22:40 +0300

    1

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from faker import Faker
+import random
+import json
+from conf import MODEL
 
 
-# Press the green button in the gutter to run the script.
+def generator(pk):
+    fake = Faker()
+    with open('books.txt', 'r') as file:
+        lines = file.readlines()
+        book = random.choice(lines)
+    title = book
+    year = random.randint(1900, 2022)
+    pages = random.randint(100, 1000)
+    isbn13 = (fake.numerify(text='%%%-%-%%%%-%%%%-%'))
+    rating = random.uniform(0, 5)
+    price = random.uniform(100, 1000)
+    author = fake.name()
+    dict_ = {
+        "model": MODEL,
+        "pk": 1,
+        "fields": {
+            "title": title,
+            "year": year,
+            "pages": pages,
+            "isbn13": isbn13,
+            "rating": rating,
+            "price": price,
+            "author": author
+        }
+    }
+    pk += 1
+    yield dict_
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    books_list = []
+    for pk in range(1, 101, 1):
+        books = generator(pk)
+        books_list.append(books)
+    print(books_list)
+with open("books.json", "w") as write_file:
+    json.dump(books_list, write_file)
