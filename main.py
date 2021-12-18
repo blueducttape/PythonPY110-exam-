3e5e02d22aca7f86f7f280b7b87ef7cf8919beb4
uliana <val.uliana@yandex.ru>
2021-12-18 16:49:38 +0300

    2

--- main.py
+++ main.py
@@ -4,40 +4,60 @@ import json
 from conf import MODEL
 
 
-def generator(pk):
-    fake = Faker()
+def book_name():
     with open('books.txt', 'r') as file:
         lines = file.readlines()
         book = random.choice(lines)
     title = book
+
+
+def year():
     year = random.randint(1900, 2022)
+
+
+def pages():
     pages = random.randint(100, 1000)
+
+
+def isbn():
+    fake = Faker()
     isbn13 = (fake.numerify(text='%%%-%-%%%%-%%%%-%'))
+
+def rating():
     rating = random.uniform(0, 5)
+
+
+def price():
     price = random.uniform(100, 1000)
+
+
+def author():
+    fake = Faker()
     author = fake.name()
+
+
+def generator():
     dict_ = {
         "model": MODEL,
         "pk": 1,
         "fields": {
-            "title": title,
-            "year": year,
-            "pages": pages,
-            "isbn13": isbn13,
-            "rating": rating,
-            "price": price,
-            "author": author
+            "title": book_name(),
+            "year": year(),
+            "pages": pages(),
+            "isbn13": isbn(),
+            "rating": rating(),
+            "price": price(),
+            "author": author()
         }
     }
-    pk += 1
     yield dict_
+    print(dict_)
 
 
 if __name__ == '__main__':
     books_list = []
     for pk in range(1, 101, 1):
-        books = generator(pk)
+        books = generator()
         books_list.append(books)
-    print(books_list)
 with open("books.json", "w") as write_file:
     json.dump(books_list, write_file)
